4db5d10b26e16e12471fb76eb3dbe3eecdc2c45c
evlc <dodgedodgedoggy@gmail.com>
2024-12-19 15:09:43 +0700

    15: make N_CONSUMERS changeable via argv

--- 15.py
+++ 15.py
@@ -1,9 +1,13 @@
 import threading
 import queue
 import numpy as np
+import sys
 
 CAP = 500
-N_CONSUMERS = 4
+N_CONSUMERS = 1
+
+if len(sys.argv) > 1:
+    N_CONSUMERS = int(sys.argv[1])
 
 
 class Producer(threading.Thread):
@@ -39,36 +43,16 @@ class Consumer(threading.Thread):
             self.task_queue.task_done()
 
 
-def main():
-    task_queue = queue.Queue()
-    lock = threading.Lock()
+task_queue = queue.Queue()
+lock = threading.Lock()
 
-    producer = Producer(task_queue, lock)
-    consumers = [Consumer(task_queue, lock) for _ in range(N_CONSUMERS)]
+producer = Producer(task_queue, lock)
+consumers = [Consumer(task_queue, lock) for _ in range(N_CONSUMERS)]
 
-    producer.start()
-    for consumer in consumers:
-        consumer.start()
+producer.start()
+for consumer in consumers:
+    consumer.start()
 
-    producer.join()
-    for consumer in consumers:
-        consumer.join()
-
-
-if __name__ == "__main__":
-    main()
-
-
-# benchmarking:
-# $ hyperfine 'python 15.py' './python313_nogil 15.py'
-# Benchmark 1: python 15.py
-#   Time (mean ± σ):     16.758 s ±  1.514 s    [User: 35.490 s, System: 0.594 s]
-#   Range (min … max):   15.461 s … 19.976 s    10 runs
-
-# Benchmark 2: ./python313_nogil 15.py
-#   Time (mean ± σ):     11.918 s ±  0.633 s    [User: 34.283 s, System: 0.316 s]
-#   Range (min … max):   10.919 s … 12.620 s    10 runs
-
-# Summary
-#   ./python313_nogil 15.py ran
-#     1.41 ± 0.15 times faster than python 15.py
+producer.join()
+for consumer in consumers:
+    consumer.join()
