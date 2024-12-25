58f0c2ecaddb26efbb1d0d37327ab333353b3c22
evlc <dodgedodgedoggy@gmail.com>
2024-12-25 14:49:55 +0700

    fix 10

--- 10.py
+++ 10.py
@@ -1,12 +1,19 @@
 def singleton(cls):
     instances = {}
 
+    def dummy(*args, **kwargs):
+        pass
+
     class SingletonWrapper(cls):
         def __new__(cls, *args, **kwargs):
             if cls not in instances:
                 instance = super().__new__(cls)
-                cls.__init__(instance, *args, **kwargs)
                 instances[cls] = instance
+            else:
+                cls.__init__ = dummy
             return instances[cls]
 
     return SingletonWrapper
+
+
+## test on classes woth prints in inits
